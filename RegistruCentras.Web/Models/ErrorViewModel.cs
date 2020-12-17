ad11bce477e2abc3c42e936433ad871594132397
aurimielisss <abajorunas@yahoo.com>
2020-12-17 09:54:27 +0200

    Update ErrorViewModel.cs

--- RegistruCentras.Web/Models/ErrorViewModel.cs
+++ RegistruCentras.Web/Models/ErrorViewModel.cs
@@ -7,5 +7,6 @@ namespace RegistruCentras.Web.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool JustTest {get;set;}
     }
 }
